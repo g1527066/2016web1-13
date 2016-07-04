7ea74a4e6d4a8063d58badafe547e11ff4e50279
nyanko-sankaku <g1527066@st.t-kougei.ac.jp>
2016-07-04 18:37:50 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -88,3 +88,10 @@ if(filename==='otherhtml'){
   
 }
 opt.selected=true;
+
+var thmubs=document.querySelectorAll('.thumb');
+for(idx in thmubs){
+  thmubs[idx].onclick=function(){
+    document.getElementById("bigimg").src='img/'this.dataset.image+'.jpg';
+  }
+}
