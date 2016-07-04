c91c1a7bc95d8ae216ebfde83acd32aeaf3abd5f
nyanko-sankaku <g1527066@st.t-kougei.ac.jp>
2016-07-04 17:17:13 +0900

    Create default.js

---
+++ js/default.js
@@ -0,0 +1,17 @@
+var separate_time=function(time){
+  var sec=Math.floor((time/1000)%60);
+  var min=Math.floor((time/1000/60)%60);
+  var hours=Math.floor((time/1000/60/60)%24);
+  var days=Math.floor(time/1000/60/60/24);
+  return [sec,min,hours,days];
+  }
+  var update=function(){
+  refresh();//タイマー起動
+  }
+  
+  
+  var refresh=function(){
+  setTimeout(update,1000);//1000ミリ秒待ってからupdateを実行
+  }
+  update();//最初の更新
+  
